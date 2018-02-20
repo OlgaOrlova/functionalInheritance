cbd9ca005ecba41efdcb02cab926c01ed4c6a888
OlgaOrlova <olgaviktorovnaorlova@gmail.com>
2018-02-20 11:16:03 +0200

    fix

--- prototypes/task1.js
+++ prototypes/task1.js
@@ -1,33 +1,33 @@
 function Shape(centerX, centerY) {
-    this.x = centerX;
-    this.y = centerY;
+    this._x = centerX;
+    this._y = centerY;
 }
 Shape.prototype.info = function () {
-    console.log("x:" + this.x + "   " + "y:" + this.y);
+    console.log("x:" + this._x + "   " + "y:" + this._y);
 }
 Shape.prototype.getX = function () {
-    return this.x;
+    return this._x;
 }
 Shape.prototype.getY = function () {
-    return this.y;
+    return this._y;
 }
 function Rectangle(x, y, diagonal) {
     Shape.call(this, x, y);
-    this.diagonal = diagonal;
+    this._diagonal = diagonal;
 }
 Rectangle.prototype = Object.create(Shape.prototype);
 Rectangle.prototype.constructor = Rectangle;
 Rectangle.prototype.info = function () {
-    console.log("x:" + this.x + "   " + "y:" + this.y + "  " + this.diagonal);
+    console.log("x:" + this._x + "   " + "y:" + this._y + "  " + "diagonal:"+ "  " + this._diagonal);
 };
 function Circle(x, y, radius) {
     Shape.call(this, x, y);
-    this.radius = radius;
+    this._radius = radius;
 }
 Circle.prototype = Object.create(Shape.prototype);
 Circle.prototype.constructor = Circle;
 Circle.prototype.info = function () {
-    console.log("x:" + this.x + "   " + "y:" + this.y + "  " + this.radius);
+    console.log("x:" + this._x + "   " + "y:" + this._y + "  " + "radius:"+ "  " + this._radius);
 };
 function main() {
     var rectangle = new Rectangle(1, 2, 3);
